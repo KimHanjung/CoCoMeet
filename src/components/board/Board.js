c933efb98a0f947bfeb4fce041e507424c621ebd
sunny <ricklee1998@naver.com>
2020-09-18 18:37:28 +0900

    add board feels

--- src/components/board/Board.js
+++ src/components/board/Board.js
@@ -1,7 +1,28 @@
-import React from 'react';
-
-function Board() {
-  return <div>Board Box</div>
+import React, { Component } from 'react';
+import SortableTree from 'react-sortable-tree';
+import 'react-sortable-tree/style.css';
+class Board extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      treeData: [
+        { title: '소공', children: [{ title: '이창열' }] },
+        { title: 'bang', children: [{ title: '방승연' }] },
+        { title: 'doing', children: [{ title: '김한중' }] },
+      ]
+    };
+  }
+  render() {
+    return (
+      <div style={{ height: 400 }}>
+        Board Box
+        <SortableTree
+          treeData={this.state.treeData}
+          onChange={treeData => this.setState({ treeData })}
+        />
+      </div>
+    );
+  }
 }
 
 export default Board;
