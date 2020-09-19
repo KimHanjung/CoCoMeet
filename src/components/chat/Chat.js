fe268b7106a6988439e6ae9cf966f7497ad08719
KimHanjung <rlagkswnd23@gmail.com>
2020-09-19 23:02:04 +0900

    chatting 틀 추가

--- src/components/chat/Chat.js
+++ src/components/chat/Chat.js
@@ -1,7 +1,22 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-function Chat() {
-  return <div>Chat Box</div>
-}
-
-export default Chat;
+  class AppChannel extends Component {
+    updateChannel(event){
+      if(event.target.value!==this.props.channel) this.props.onUpdate(event.target.value);
+    }
+    keyUpdateChannel(event){
+      if(event.keyCode===13) {
+          if(event.target.value!==this.props.channel) this.props.onUpdate(event.target.value);
+      }
+    }
+    render() {
+      return (
+          <header className="Channel-header">
+            <h1 className="Channel-title">Welcome to Talkwith</h1>
+            <input type="text" className="channel" placeholder="channel name" onBlur={this.updateChannel.bind(this)} onKeyDown={this.keyUpdateChannel.bind(this)}/>
+          </header>
+      );
+    }
+  }
+  
+  export default AppChannel;
