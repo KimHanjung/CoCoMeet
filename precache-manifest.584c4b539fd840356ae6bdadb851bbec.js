03cdb25b03845016e3be276253c083594cc69c2c
Chang-yeol <777john@yonsei.ac.kr>
2020-09-22 13:03:13 +0900

    build

---
+++ precache-manifest.584c4b539fd840356ae6bdadb851bbec.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "890fa8c0d29c7c70a146e58512a95ace",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aa9c5456ab4657e17363",
+    "url": "/static/css/2.da458d89.chunk.css"
+  },
+  {
+    "revision": "a5c24869af90a64bb509",
+    "url": "/static/css/main.a35c36ea.chunk.css"
+  },
+  {
+    "revision": "aa9c5456ab4657e17363",
+    "url": "/static/js/2.d365488d.chunk.js"
+  },
+  {
+    "revision": "b67bc97fd0dd67004287af4028afc1b1",
+    "url": "/static/js/2.d365488d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a5c24869af90a64bb509",
+    "url": "/static/js/main.23d9c64b.chunk.js"
+  },
+  {
+    "revision": "84d2641b8b68198eb97b",
+    "url": "/static/js/runtime-main.8a0e17a3.js"
+  },
+  {
+    "revision": "079078d78ac4466fbd9cb203ceba6eb1",
+    "url": "/static/media/chuchuchu.079078d7.jpg"
+  }
+]);
