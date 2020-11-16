a8c09a6e37608bd76339c22ed2de00d38fa141d9
방승연 <baaaang_53@yonsei.ac.kr>
2020-11-16 22:11:48 +0900

    Virtual database in .js format

---
+++ src/DB/DB1_tree.js
@@ -0,0 +1,63 @@
+export const Total_TREES = [
+    {
+        room_id : 0,
+        tree_id : 0,
+        node_id : 0,
+        title : "node_0",
+        parent : null, 
+        color : "blue", 
+        deco : "normal",
+        weight : "normal"
+    },
+    {
+        room_id : 0,
+        tree_id : 0,
+        node_id : 1,
+        title : "node_1",
+        parent : 0, 
+        color : "blue", 
+        deco : "normal",
+        weight : "normal"
+    },
+    {
+        room_id : 0,
+        tree_id : 0,
+        node_id : 2,
+        title : "node_2",
+        parent : 0, 
+        color : "blue", 
+        deco : "normal",
+        weight : "normal"
+    },
+    {
+        room_id : 0,
+        tree_id : 0,
+        node_id : 3,
+        title : "node_3",
+        parent : 2, 
+        color : "blue", 
+        deco : "normal",
+        weight : "normal"
+    },
+    {
+        room_id : 0,
+        tree_id : 1,
+        node_id : 4,
+        title : "node_4",
+        parent : null, 
+        color : "blue", 
+        deco : "normal",
+        weight : "normal"
+    },
+    {
+        room_id : 0,
+        tree_id : 1,
+        node_id : 5,
+        title : "node_5",
+        parent : 4, 
+        color : "blue", 
+        deco : "normal",
+        weight : "normal"
+    }
+];
+
---
+++ src/DB/DB2_order.js
@@ -0,0 +1,53 @@
+export const Total_ORDERS = [
+    {
+        room_id : 0,
+        tree_id : 0,
+        content : "0-tree0-order"
+    },
+    {
+        room_id : 0,
+        tree_id : 1,
+        content : "0-tree1-order"
+    },
+    {
+        room_id : 0,
+        tree_id : 2,
+        content : "0-tree2-order"
+    },
+    {
+        room_id : 0,
+        tree_id : 3,
+        content : "0-tree3-order"
+    },
+    {
+        room_id : 0,
+        tree_id : 4,
+        content : "0-tree4-order"
+    },
+    {
+        room_id : 1,
+        tree_id : 0,
+        content : "1-tree0-order"
+    },
+    {
+        room_id : 1,
+        tree_id : 1,
+        content : "1-tree1-order"
+    },
+    {
+        room_id : 1,
+        tree_id : 2,
+        content : "1-tree2-order"
+    },
+    {
+        room_id : 1,
+        tree_id : 3,
+        content : "1-tree3-order"
+    },
+    {
+        room_id : 1,
+        tree_id : 4,
+        content : "1-tree4-order"
+    }
+];
+
