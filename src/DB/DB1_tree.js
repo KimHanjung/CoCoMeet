46976a4f12e6d2dcf9a797c409f0fa9183896cb0
방승연 <baaaang_53@yonsei.ac.kr>
2020-11-17 00:53:06 +0900

    블록추가, 블록삭제, suns-apple 완료  일단 db 파일에서 export 함수 형태로 값 변경 중

--- src/DB/DB1_tree.js
+++ src/DB/DB1_tree.js
@@ -61,3 +61,35 @@ export let Total_TREES = [
     }
 ];
 
+// new Tree는 결국 tree_id가 새 거인 node를 만드는 것과 같음
+export const newTree = (room_id) => {
+
+}
+
+export const newApple = (room_id, tree_id, text, parent) => {
+    const roomNodes = Total_TREES.filter(block => block.room_id === room_id);
+    const newNode = {
+        room_id : room_id,
+        tree_id : tree_id, 
+        node_id : roomNodes.length, 
+        title : text, 
+        parent : parent,
+        color : "blue",
+        deco : "normal",
+        weight : "normal"
+    }
+    Total_TREES.push(newNode);
+    return newNode;
+}
+
+export const deleteBlock = (room_id, node_id) => {
+    // 지금은 cleaned_ 로 재생성 후 업데이트. Redis에서의 del로 변경 예정
+    const cleanedBlocks = Total_TREES.filter(block => block.room_id !== room_id || block.node_id !== node_id);
+    if (Total_TREES.length > cleanedBlocks.length) {
+        Total_TREES = cleanedBlocks;
+        return true;
+    }
+    else {
+        return false;
+    }
+}
--- src/server/resolver.js
+++ src/server/resolver.js
@@ -1,13 +1,27 @@
 import {Total_ORDERS} from "./DB2_order";
+import {Total_TREES, newApple, deleteBlock} from "./DB1_tree";
 
 const resolvers = {
     Query: {
-        get_order: (_, {room_id, tree_id}) => getById(room_id, tree_id),
-        orders : () => Total_ORDERS
+        get_order: (_, {room_id, tree_id}) => getOrderById(room_id, tree_id),
+        orders : () => Total_ORDERS,
+        get_tree:(_, {room_id, tree_id}) => getTree(room_id, tree_id),
+    },
+    Mutation: {
+        delete_block: (_, {room_id, node_id}) => deleteBlock(room_id, node_id),
+        new_apple: (_, {room_id, tree_id, text, parent}) => newApple(room_id, tree_id, text, parent), 
+        new_block:(_, {room_id, tree_id}) => newApple(room_id, tree_id, "default_text", null)
     }
 };
 
-const getById = (room_id, tree_id) => {
+// DataBase1. Tree
+const getTree = (room_id, tree_id) => {
+    const blocks = Total_TREES.filter(block => block.room_id === room_id && block.tree_id === tree_id);
+    return blocks;
+}
+
+// DataBase2. Order
+const getOrderById = (room_id, tree_id) => {
     const filteredorders = Total_ORDERS.filter(order => order.room_id === room_id && order.tree_id === tree_id);
     return filteredorders[0];
 }
