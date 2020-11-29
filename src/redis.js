fa5052f2a36efcf53c15fc8cc79f495cab8a467d
방승연 <baaaang_53@yonsei.ac.kr>
2020-11-29 20:53:40 +0900

    1097 commits

---
+++ src/redis.js
@@ -0,0 +1,13 @@
+var redis = require('redis');
+
+var redisClient = redis.createClient({port}, {host});
+
+redisClient.auth({password}, function(err) {
+    if (err) throw err;
+}) ;
+
+redisClient.on('error', function(err){
+    console.log('Redis error: ' + err);
+});
+
+module.export = redisClient;
